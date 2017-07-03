f557efbda9f217d0dac9c98b2f0812fe0cef702d
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2017-07-03 02:39:59 +0100

    adding initial class + tests.

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -2,10 +2,23 @@
 
 const GlobalConnection = require('../lib');
 
-describe('Main', () => {
-    it('must instantiate', () => {
-        const a = new GlobalConnection();
-        expect(a).toBeTruthy();
-    });
+class Database {
+    constructor() {
+        this.$config = {
+            version: '1.2.3'
+        };
+    }
+}
 
+describe('Construction', () => {
+    it('must throw on invalid driver', () => {
+        expect(() => {
+            new GlobalConnection();
+        }).toThrow(new TypeError('Parameter \'db\' is invalid.'));
+    });
+    it('must throw on invalid driver version', () => {
+        expect(() => {
+            new GlobalConnection(new Database());
+        }).toThrow(new TypeError('Minimum pg-promise supported is 6.3.0'));
+    });
 });
